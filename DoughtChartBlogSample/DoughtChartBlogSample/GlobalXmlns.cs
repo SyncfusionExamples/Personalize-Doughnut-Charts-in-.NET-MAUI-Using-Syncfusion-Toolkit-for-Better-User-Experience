a403ce1a6c2879de8b5b8779a8c8ca530c9dcd02
Baranibharathip <162285848+Baranibharathip@users.noreply.github.com>
2025-09-12 17:25:56 +0530

    removed sample

--- DoughtChartBlogSample/DoughtChartBlogSample/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DoughtChartBlogSample")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DoughtChartBlogSample.Pages")]
